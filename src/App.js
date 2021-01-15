f83fa0913cf97ba633bf4fc0bdc6b2a46f38918d
sayhan2021 <schowdhury0250@richmondhillhs.org>
2021-01-15 19:10:19 +0000

    NYS LAB

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
-// Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+ import SchoolCard from './components/SchoolCard.js';
+ import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SchoolCard school={data[0]} />
+      <SchoolCard school={data[1]} />
+      <SchoolCard school={data[2]} />
+      <SchoolCard school={data[3]} />
       <div className="container">
         <div className="row">
 
